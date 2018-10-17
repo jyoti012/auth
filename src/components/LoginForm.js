f3a38d3b8fef544ca154ceed3747ede0cc31cb1f
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-17 15:47:54 +0530

    Input and button fields ready

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-	state = { text: ''}
+	state = { email: '', password: ''}
 
 	render() {
 		return (
 			<Card>
-				<CardSection />
-					<TextInput 
-						value={this.state.text}
-						onChangeText={text => this.setState({ text })}
-						style={{ height: 20, width: 100 }}/>
-				<CardSection />
-
 				<CardSection>
-					<Button>
+					<Input 
+						placeholder="johndoe@gmail.com"
+						label="Email"
+						value={this.state.email}
+						onChangeText={email => this.setState({ email })} />
+				</CardSection>
+				<CardSection>
+				<Input 
+					placeholder="aA@qw12"
+					label="Password"
+					secureTextEntry
+					value={this.state.password}
+					onChangeText={password => this.setState({ password })} />
+			</CardSection>
+				<CardSection>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log In
 					</Button>
 				</CardSection>
-
 			</Card>
 		);
 	}
