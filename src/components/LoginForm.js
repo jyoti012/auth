b802ab765857711709e6b8cafe584f1a935052a2
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-17 18:42:38 +0530

    Login auth success

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
-	state = { email: '', password: ''}
+	state = { email: '', password: '', error: '' };
+
+	onButtonPress() {
+		const { email, password } = this.state;
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(() => {
+				console.log('firebase success');
+			})
+			.catch(() => {
+				console.log('firebase error');
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(() => {
+						console.log("User registered");
+					})
+					.catch(() => {
+						console.log("Auth err")
+						this.setState({ error: 'Authentication Failed' });
+					});
+			});
+	}
 
 	render() {
 		return (
@@ -14,14 +35,20 @@ class LoginForm extends Component {
 						value={this.state.email}
 						onChangeText={email => this.setState({ email })} />
 				</CardSection>
+
 				<CardSection>
-				<Input 
-					placeholder="aA@qw12"
-					label="Password"
-					secureTextEntry
-					value={this.state.password}
-					onChangeText={password => this.setState({ password })} />
-			</CardSection>
+					<Input 
+						placeholder="aA@qw12"
+						label="Password"
+						secureTextEntry
+						value={this.state.password}
+						onChangeText={password => this.setState({ password })} />
+				</CardSection>
+
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
 						Log In
@@ -29,6 +56,14 @@ class LoginForm extends Component {
 				</CardSection>
 			</Card>
 		);
+	}
+}
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
 	}
 }
 
